9155c3246c1f4a09645e3fef7df75b368bf63100
Jim Strother <jim.strother@gmail.com>
2020-04-11 14:41:32 -0700

    Added: v-intersect example in Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -23,6 +23,26 @@
         <EventTimeline :timeline="timeline" />
       </v-col>
     </v-row>
+    <!-- the table and timeline were duplicated to demonstrate v-intersect -->
+
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
 
     <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
@@ -56,6 +76,7 @@ export default {
   data() {
     return {
       employees: employeesData,
+      loadNewContent: false,
       sales: salesData,
       selectedEmployee: {
         name: '',
@@ -71,6 +92,9 @@ export default {
       this.snackbar = true
       this.selectedEmployee.name = event.name
       this.selectedEmployee.title = event.title
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting;
     },
   },
 }
